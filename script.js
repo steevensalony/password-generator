39c0873c73cc49faa5372a75aafa174067bf6834
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-01-17 17:56:50 -0500

    Added functions to choose random characters

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getUpperCase() {
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+  }
+  
+  function getLowerCase() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  }
+  function getNumber() {
+  var number = "0123456789"
+  return number[Math.floor(Math.random() * number.length)];
+  }
+  function getSpecialCharacters() {
+  var specialCharacters = "!@#$%^&*(){}[]=<>/,.";
+  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  }
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
